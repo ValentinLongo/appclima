e46dcca1e0ba5d67967b60fa9fc8b58938c2bcc7
ValentinLongo <longovalentin@gmail.com>
2022-03-26 17:26:29 -0300

    css

--- src/Get.jsx
+++ src/Get.jsx
@@ -3,7 +3,10 @@ import Styles from "./Get.module.css"
 export function Get(){
     return(
         <div className={Styles.get}>
-
+            <p>Location:</p>
+            <p>Temperature:</p>
+            <p>Humidity:</p>
+            <p>Wind Speed:</p>
         </div>
     )
 }
--- src/Set.jsx
+++ src/Set.jsx
@@ -3,8 +3,8 @@ import Styles from "./Set.module.css"
 export function Set(){
     return(
     <div className={Styles.set}>
-        <input type="text" className={Styles.input}/>
-        <input type="text" className={Styles.input}/>
+        <input type="text" className={Styles.input} placeholder="Ciudad"/>
+        <input type="text" className={Styles.input} placeholder="Pais"/>
         <button className={Styles.button}>Aceptar</button>
     </div>)
 }
